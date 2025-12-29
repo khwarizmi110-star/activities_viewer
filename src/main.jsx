aac6bf484f4568a5fe379139ba3ee1845a1038b9
khwarizmi110-star <khwarizmi110@gmail.com>
2025-12-30 00:03:44 +0300

    sjhs

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 import App from './App.jsx'
 import './index.css'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
             <App />
+            <Analytics />
         </Provider>
     </React.StrictMode>,
 )
